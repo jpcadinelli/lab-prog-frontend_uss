1147094703258bc6b0f6eb54a8690e95a38e34d3
jpcadinelli <jpcadinelli@gmail.com>
2024-05-12 15:37:01 -0300

    :recycle: refactor: separando os models

---
+++ congresso-saude-e-bem-estar/models/palestra.py
@@ -0,0 +1,9 @@
+class Palestra:
+    def __init__(self, id, nome, descricao, dia, mes, palestrantes, foto):
+        self.id = id
+        self.nome = nome
+        self.descricao = descricao
+        self.dia = dia
+        self.mes = mes
+        self.palestrantes = palestrantes
+        self.foto = foto
