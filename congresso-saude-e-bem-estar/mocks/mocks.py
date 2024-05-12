dc48a821d9064a4245fd47e0116f11c95b397450
jpcadinelli <jpcadinelli@gmail.com>
2024-05-12 15:37:47 -0300

    :sparkles: feat: criando os primeiros mocks para teste

---
+++ congresso-saude-e-bem-estar/mocks/mocks.py
@@ -0,0 +1,21 @@
+from ..models.palestra import Palestra
+from ..models.palestrantes import Palestrante
+
+# Criando das Palestras
+palestra1 = Palestra(
+    '7166198a-baf6-4e91-b144-942d818d4f42',
+    'Qualidade de vida e bem-estar.',
+    'A palestra "Qualidade de Vida e Bem-Estar" ministrada pelo Dr. Drauzio Varella é uma oportunidade valiosa de explorar as complexidades da saúde e do bem-estar humano. Com seu conhecimento vasto e acessível, o Dr. Drauzio aborda diversos aspectos que influenciam a qualidade de vida, desde hábitos saudáveis de alimentação e exercício físico até questões emocionais e sociais.\nAo longo da palestra, o Dr. Drauzio compartilha insights baseados em evidências científicas sobre como pequenas mudanças no estilo de vida podem ter um impacto significativo na saúde geral e no bem-estar emocional das pessoas. Ele também aborda a importância da prevenção de doenças, destacando a necessidade de exames regulares e cuidados preventivos.\nAlém disso, o Dr. Drauzio explora temas como o equilíbrio entre trabalho e vida pessoal, a importância do sono adequado e estratégias para lidar com o estresse cotidiano. Sua abordagem envolvente e inspiradora motiva os espectadores a assumirem o controle de sua saúde e a adotarem hábitos que promovam uma vida mais plena e saudável.',
+    22,
+    'Maio',
+    '372eba98-5b14-489a-9607-f54f3842e476',
+    'qualidade-de-vida-e-bem-estar.png'
+)
+
+# Criando dos Palestrantes
+palestrante1 = Palestrante(
+    '372eba98-5b14-489a-9607-f54f3842e476',
+    'Dr. Drauzio Varella',
+    'O Dr. Drauzio Varella é um renomado médico brasileiro, conhecido não apenas por sua atuação na medicina, mas também por seu trabalho como escritor, cientista e comunicador. Nascido em São Paulo em 1943, Drauzio graduou-se em Medicina pela Universidade de São Paulo (USP) em 1970, especializando-se em oncologia.\nSua trajetória profissional é marcada por uma notável dedicação à saúde pública e à divulgação científica. Drauzio Varella tornou-se amplamente conhecido por seu trabalho em prol da conscientização sobre doenças como HIV/AIDS, hepatite C e câncer, além de temas relacionados à saúde da população carcerária, área em que desenvolveu projetos pioneiros.\nAlém de sua atuação como médico e pesquisador, Drauzio é um prolífico autor, tendo escrito diversos livros que abordam temas médicos e sociais de forma acessível e informativa. Entre suas obras mais famosas estão "Estação Carandiru", que narra suas experiências como médico voluntário no extinto Complexo Penitenciário do Carandiru, em São Paulo, e "Por um Fio", que trata de questões relacionadas à medicina e à vida humana.\nDrauzio Varella também é reconhecido por seu trabalho na mídia, tendo apresentado programas de televisão e colaborado com diversos veículos de comunicação para disseminar informações relevantes sobre saúde e qualidade de vida.\nSua habilidade em comunicar informações complexas de forma clara e acessível o tornou uma figura muito respeitada e querida pelo público brasileiro. Drauzio Varella é um exemplo de como a medicina pode transcender os consultórios e hospitais, alcançando um papel fundamental na educação e na promoção da saúde pública.',
+    'dr-drauzio-varella.png'
+)
