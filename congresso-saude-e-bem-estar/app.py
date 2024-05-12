bd94784e0378587db7ce7a0a0ea5c3a4652b32e3
jpcadinelli <jpcadinelli@gmail.com>
2024-05-12 15:56:28 -0300

    :sparkles: feat: passando os mocks para o index

--- congresso-saude-e-bem-estar/app.py
+++ congresso-saude-e-bem-estar/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
+from mocks.mocks import palestras, palestrantes
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", palestras=palestras, palestrantes=palestrantes)
