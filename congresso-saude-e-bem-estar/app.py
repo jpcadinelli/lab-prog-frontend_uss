bdb2e1d792f20bd4e9c191e5dde7ea6a6fa0813c
jpcadinelli <jpcadinelli@gmail.com>
2024-05-13 16:30:22 -0300

    :sparkles: feat: adiconando nova rota

--- congresso-saude-e-bem-estar/app.py
+++ congresso-saude-e-bem-estar/app.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html", palestras=palestras, palestrantes=palestrantes)
+
+@app.route("/ministrantes/<int:id>")
+def ministrantes(id:int):
+    for palestra in palestras:
+        if palestra.id == id:
+            return render_template("palestrantes.html", palestra=palestra, palestrantes=palestrantes)
+    return "<h1>404 Page Not Found.</h1>"
