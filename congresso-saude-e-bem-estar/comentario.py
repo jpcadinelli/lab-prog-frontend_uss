1e9b1321059a200f96f7f207859fe3cb1f7191db
jpcadinelli <jpcadinelli@gmail.com>
2024-05-13 22:23:03 -0300

    :sparkles: feat: adição dos comentários

---
+++ congresso-saude-e-bem-estar/comentario.py
@@ -0,0 +1,6 @@
+class Comentario:
+    def __init__(self, id, nome, comentario, idPalestra):
+        self.id = id
+        self.nome = nome
+        self.comentario = comentario
+        self.idPalestra = idPalestra
